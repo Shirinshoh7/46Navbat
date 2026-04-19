1e31277d2f0cf1bd8f0e550eba963fb5643b2ec7
shirinshosh <badalovshirinshoh29@gmail.com>
2026-04-19 12:49:22 +0900

    add Readme

--- obj/Debug/net9.0/46Navbat.AssemblyInfo.cs
+++ obj/Debug/net9.0/46Navbat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("46Navbat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8075d4c18ab9e8f0df0ef349c62e4adf08ae247a")]
 [assembly: System.Reflection.AssemblyProductAttribute("46Navbat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("46Navbat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
